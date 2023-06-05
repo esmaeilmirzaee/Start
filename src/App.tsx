7e8821ff7a72ff54cb23b3b263e6ae60c91533ff
esmaeilmirzaee <esmaeilmirzaee@gmail.com>
2023-06-05 12:24:32 +0330

    2 files modified, 1 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { useState } from 'react';
+import { Context } from './utils/contexts';
+import { Blog } from './components/index';
 
 function App() {
   const [count, setCount] = useState(0);
+  const value = 5;
 
   return (
-    <div className="flex justify-center items-center w-screen h-screen">
-      <h1 className="text-center font-bold text-6xl">Start</h1>
-    </div>
+    <Context.Provider value={value}>
+      <div className="flex justify-center items-center w-screen h-screen">
+        <h1 className="text-center font-bold text-6xl">Start</h1>
+        <div className="text-indigo-800">
+          <Blog />
+        </div>
+      </div>
+    </Context.Provider>
   );
 }
 
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,1 +1,14 @@
+import { useContext } from 'react';
+import { Context } from '../utils/contexts';
+
 // All new components will come here and spread in the application
+export function Blog() {
+  const value = useContext(Context);
+
+  return (
+    <div>
+      <h1>Now I'm using context</h1>
+      <h6>{value}</h6>
+    </div>
+  );
+}
