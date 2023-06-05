d4a33cbd7da11f03d9825cf5f5d6c3ff2086a8c7
esmaeilmirzaee <esmaeilmirzaee@gmail.com>
2023-06-05 11:53:55 +0330

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,7 @@ function App() {
 
   return (
     <div className="flex justify-center items-center w-screen h-screen">
-      <div></div>
-      <h1>Vite + React</h1>
-      <div className="bg-black">
-        <button
-          className="bg-indigo-600 text-white"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="text-center font-bold text-6xl">Start</h1>
     </div>
   );
 }
